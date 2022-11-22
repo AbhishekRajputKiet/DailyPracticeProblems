c6a7071b38ac28b1b33ea3f8df52d8d37e7ceed2
Abhishek Rajput <abhishek0070293@gmail.com>
2022-11-22 16:23:03 +0530

    sumoftwo number program is added

---
+++ addTwoMNum.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter two numbers : "<<endl;
+    cin>>a>>b;
+    cout<<"the sum of "<<a<<" and "<<b<<" is : "<<(a+b);
+    return 0;
+}
