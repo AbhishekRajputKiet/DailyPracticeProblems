6a317224b93d25149b81cf785474b39472c63f65
Abhishek Rajput <abhishek0070293@gmail.com>
2023-01-16 09:28:29 +0530

    Tree_8_RightView_Of_Tree

---
+++ PracticeCode/practice49.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    struct Node*left;
+    struct Node*right;
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+void RightView(Node* root){
+    if(root==NULL)
+        return ;
+    queue<Node*>q;
+    q.push(root);
+    
+    while(!q.empty()){
+        int n=q.size();
+        for(int i=0;i<n;i++){
+            Node* curr= q.front();
+            q.pop();
+            if(i==n-1)
+                cout<<curr->data<<" ";
+            if(curr->left != NULL)
+                q.push(curr->left);
+            if(curr->right != NULL)
+                q.push(curr->right);
+        }
+    }
+}
+
+
+int main(){
+    /*          1 
+              /   \
+             2     3
+            / \   / \
+           4   5 6   7
+    */
+    Node* root= new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+    root->right->left=new Node(6);
+    root->right->right=new Node(7);
+    RightView(root);
+
+    return 0;
+}
