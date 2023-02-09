225a7fc040452a884ab87760b42ebbcc5439dd68
Abhishek Rajput <abhishek0070293@gmail.com>
2023-02-09 23:53:16 +0530

    codechef problem using vector

---
+++ codechef/vector_5.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printv(vector<int>&v){
+    cout<<"size: "<<v.size()<<endl;
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    int N;
+    cin>>N;
+    vector<int>v[N];
+    for(int i=0;i<N;i++){
+        int n;
+        cin>>n;
+        for(int j=0;j<n;j++){
+            int x;
+            cin>>x;
+            v[i].push_back(x);
+        }
+    }
+    for(int i=0;i<N;i++){
+        printv(v[i]);
+    }
+    return 0;
+}
