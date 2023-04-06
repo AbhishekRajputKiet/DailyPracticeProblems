ed3e315b35c302946c1acb0a009baaf82877cdbd
Abhishek Rajput <abhishek0070293@gmail.com>
2023-04-06 08:02:33 +0530

    Recursion Move all x on right

---
+++ codechef/recursion_11.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+string moveallx(string s){
+    if(s.length()==0)
+        return "";
+    char ch=s[0];
+    string ans=moveallx(s.substr(1));
+    if(ch=='x')
+        return ans+ch;
+    return ch+ans;
+
+}
+int main(){
+    cout<<moveallx("axbabexxriethx");
+    return 0;
+}
