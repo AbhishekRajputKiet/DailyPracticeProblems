8fb83164a42cf8af9f803285823413bdc0ce9929
Abhishek Rajput <abhishek0070293@gmail.com>
2023-04-06 17:47:48 +0530

    Recursion KeyPad

---
+++ codechef/recursion_14.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+string keypadarr[]={"","","abc","def","ghi","jkl","mno","pqr","tuv","wxyz"};
+void keypad(string s,string ans){
+    if(s.length()==0){
+        cout<<ans<<endl;
+        return ;
+    }
+    char ch=s[0];
+    string code=keypadarr[ch-'0'];
+    string ros=s.substr(1);
+    for(int i=0;i<code.length();i++){
+        keypad(ros,ans+code[i]);
+    }
+
+}
+int main(){
+    keypad("23","");
+    return 0;
+}
