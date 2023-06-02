1febd4870237f574f5308b1391f0869458af0920
Abhishek Rajput <abhishek0070293@gmail.com>
2023-06-03 00:44:23 +0530

    Attributes 2

---
+++ codechef/string_2_attributes.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s1="Abhi";
+    string s2="shek";
+    /*
+    s1.append(s2);
+    cout<<s1<<endl;
+    
+   s1=s1+s2;
+   cout<<s1[6]<<endl;
+    
+    s1=s1+s2;
+    s1.clear();
+    cout<<s1<<endl;
+    
+   //s1.compare(s2);
+   
+   cout<<s2.compare(s1)<<endl;
+    cout<<s1.empty()<<endl;
+    */
+   string s3="abhishekrajput";
+   // first address then how mani character ;
+   /*
+   s3.erase(3,2);
+   cout<<s3<<endl;
+   */
+    sort(s3.begin(),s3.end());
+    cout<<s3<<endl;
+}
