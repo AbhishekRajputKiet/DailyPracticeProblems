15ecaada5005091f388f903c92b573f604249b82
Abhishek Rajput <abhishek0070293@gmail.com>
2023-06-02 23:40:41 +0530

    Attributes 1

---
+++ codechef/string_1_attributes.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    /*string str;
+    cin>>str;
+    cout<<str<<endl;*/
+
+    /*string str(5,'0');
+    cout<<str<<endl;*/
+
+    /*string str="Abhishek";
+    cout<<str<<endl;*/
+
+    string s;
+    getline(cin,s);
+    cout<<s<<endl;
+    cout<<s.size();
+    return 0;
+}
