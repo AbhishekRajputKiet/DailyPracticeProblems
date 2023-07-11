b41551257c0f21fea7a439c95cffcd6fb6a49306
Abhishek Rajput <abhishek0070293@gmail.com>
2023-07-12 02:09:02 +0530

    vector pair

---
+++ PracticeCode/practice74.cpp
@@ -0,0 +1,28 @@
+# include<bits/stdc++.h>
+using namespace std;
+
+void printv(vector<pair<int,int > >&v){
+    cout<<"size of v : "<<v.size()<<endl;
+    for(int i=0;i<v.size();i++){
+        cout<<v[i].first<<" "<<v[i].second<<endl;
+    }
+    cout<<endl;
+}
+
+
+
+int main(){
+    vector<pair<int,int > >v;
+    printv(v);
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int x,y;
+        cin>>x>>y;
+        v.push_back({x,y});
+
+    }
+    printv(v);
+    return 0;
+
+}
