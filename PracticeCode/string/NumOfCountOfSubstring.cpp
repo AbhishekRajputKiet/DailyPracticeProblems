c0c7d01cebab10ec55ac6216ae82315beab2fc28
Abhishek Rajput <118709229+AbhishekRajputKiet@users.noreply.github.com>
2023-09-10 00:01:17 +0530

    Number of count of substring in another given string

---
+++ PracticeCode/string/NumOfCountOfSubstring.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int substring(string s1,string s2){
+    int count=0;
+    for(int i=0;i<s1.length();i++){
+        string s3;
+        for(int j=i;j<s1.length();j++){
+            s3=s3+s1[j];
+            if(s2.find(s3)!=1)
+                count++;
+        }
+    }
+    return count;
+}
+int main()
+{
+    string s1="aab",s2="aaaab";
+    cout<<substring(s1,s2)<<endl;
+
+    return 0;
+}
