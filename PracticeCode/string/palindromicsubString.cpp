5780bbfde806f1d9b0e49f49c4b0f09dc83da8cb
Abhishek Rajput <118709229+AbhishekRajputKiet@users.noreply.github.com>
2023-09-10 10:11:36 +0530

    Palindromic SubString

---
+++ PracticeCode/string/palindromicsubString.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int MAX_CHAR=26;
+int countPalindromicSubString(string s){
+    int n=s.length();
+    int count=0;
+    // for frequency 
+    int hashTable[MAX_CHAR];
+    for(int i=0;i<n;i++){
+        hashTable[s[i]-'a']++;
+    }
+    for(int i=0;i< 26;i++){
+        if(hashTable[i]){
+            count += (hashTable[i]*(hashTable[i]+1)/2);
+        }
+    }
+    return count;
+}
+int main(){
+    string s="acbbd";
+    cout<<countPalindromicSubString(s)<<endl;
+    return 0;
+}
