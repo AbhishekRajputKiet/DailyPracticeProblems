84268eb499128952e9dfeaccd5acb0701f26508a
Abhishek Rajput <118709229+AbhishekRajputKiet@users.noreply.github.com>
2023-10-27 09:32:58 +0530

    class 1

--- PracticeCode/Hashing/hashMapString.cpp
+++ PracticeCode/Hashing/hashMapString.cpp
@@ -3,18 +3,23 @@ using namespace std;
 int main(){
     string s;
     cin>>s;
-    
+
     map<char,int>mp;
     for(int i=0;i<s.length();i++)
-        mp[s[i]-'a']++;
-    int q;
-    cin>>q;
+        mp[s[i]]++;
 
-    while(q--){
-        char ch;
-        cin>>ch;
-        cout<<mp[ch-'a']<<endl;
+    for(auto val : mp){
+        cout<<val.first<<"->"<<val.second<<endl;
     }
+    // int q;
+    // cin>>q;
+
+    // while(q--){
+    //     char ch;
+    //     cin>>ch;
+    //     cout<<mp[ch-'a']<<endl;
+    // }
+    
 
     return 0;
 }
---
+++ PracticeCode/classEmployee.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Employee{
+
+    public:
+        string name="Abhishek";
+        long long roll_number = 2100290120007;
+    
+
+    
+
+};
+int main(){
+    Employee ob;
+    cout<<ob.name<<endl;
+    cout<<ob.roll_number<<endl;
+    return 0;
+}
